ded0be481a6f4bd2ce8872bf41538faddf7cef0c
newmedl4waz <dlkhwazothman7@gmail.com>
2021-12-07 14:42:20 +0300

    i redo the note class

--- lib/models/note.dart
+++ lib/models/note.dart
@@ -1,33 +1,8 @@
 class Note {
-  int id;
   String title;
-  String description;
-  List<Note> notes;
+  bool desciption;
 
-  Note({
-    this.id = null,
-    this.title = "Note",
-    this.description = "Note",
-    List<Note> notes,
-  });
+  Note({this.title, this.desciption});
 
-  Map<String, dynamic> toMap() {
-    Map<String, dynamic> data = Map<String, dynamic>();
-    if (id != null) {
-      data['id'] = id;
-    }
-    data['title'] = title;
-    data['description'] = description;
-
-    return data;
-  }
-
-  @override
-  toString() {
-    return {
-      'id': id,
-      'title': title,
-      'description': description,
-    }.toString();
-  }
+  Note.copy(Note from) : this(title: from.title, desciption: from.desciption);
 }
